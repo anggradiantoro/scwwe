af856dde4c74c1a3f7ad86cf3027ab5cf87f8914
kamunikan <109719587+kamunikan@users.noreply.github.com>
2022-08-24 23:01:20 +0800

    Delete install-xray.sh

--- install-xray.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-
-wget https://raw.githubusercontent.com/fisabiliyusri/mantapv2/main/data-xray.sh && chmod +x data-xray.sh && ./data-xray.sh
-clear
-
